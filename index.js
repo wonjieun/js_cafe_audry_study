3bdaecd04a0e15b87e678222d21bf3e44adb0ab3
wonjieun <wonjieunkk@gmail.com>
2018-12-02 21:25:27 +0900

    12/02 mentoring

--- index.js
+++ index.js
@@ -2,202 +2,234 @@ let names = [];
 
 let sort = "ascend";
 
-// let myStorage = localStorage;
-
-let stackPeak = 0;
+let stackPeak = localStorage.length;
 
 let Index = {
-    init: () => {
-        console.log("Index start");
-        Index.draw();
-    },
-    btnCreate: e => {
-        e.preventDefault();
-        if (Index.validation()) {
-            Index.models.name = document.getElementById("viewName").value;
-            Index.models.id = Index.uuid();
-            // let filterNames = names.filter(i => {
-            //     let model = Index.setJsonDataToString(i);
-            //     return model.name != Index.models.name;
-            // });
-            names.push(Index.setJsonDataToString(Index.models));
-            console.log("JSON Data To String: "+Index.setJsonDataToString(Index.models));
-            console.log(localStorage.index0);
-            console.log(typeof localStorage.index1);
-            console.log(typeof localStorage);
-            // key 값에 '숫자'를 입력하면 원하는 값이 나오지 않음. (예. 0: "0")
-            // myStorage.setItem(Index.models.id, Index.models.name);
-            localStorage.setItem(`index${stackPeak}`, Index.setJsonDataToString(Index.models));
-            stackPeak++;
-            console.log(localStorage);
+  init: () => {
+    console.log("Index start");
+    console.log("init localStorage: " + JSON.stringify(localStorage));
+    Index.draw();
+  },
+  getElement: id => {
+    return document.getElementById(id);
+  },
+  getElementValue: id => {
+    return document.getElementById(id).value;
+  },
+  setElementValue: (id, text) => {
+    document.getElementById(id).value = text;
+  },
+  btnCreate: e => {
+    e.preventDefault();
+    if (Index.validation()) {
+      Index.models.name = Index.getElementValue("viewName");
+      Index.models.id = Index.uuid();
+      // let filterNames = names.filter(i => {
+      //     let model = Index.setJsonDataToString(i);
+      //     return model.name != Index.models.name;
+      // });
+      names.push(Index.setJsonDataToString(Index.models));
+      console.log(
+        "JSON Data To String: " + Index.setJsonDataToString(Index.models)
+      );
+      // key 값에 '숫자'를 입력하면 원하는 값이 나오지 않음. (예. 0: "0")
+      // myStorage.setItem(Index.models.id, Index.models.name);
+      localStorage.setItem(
+        `index${stackPeak}`,
+        Index.setJsonDataToString(Index.models)
+      );
+      stackPeak++;
+      console.log(localStorage);
 
-            // 숙제: Set처리하는 방법 알아보기
-            // let setNames = new Set(names);
-            // names = Array.from(setNames);
-            // console.log(">>>>>" + Index.filterNames(names, Index.models.name));
-            // names = [];
-            let newNames = Index.filterNames(names, Index.models.name);
-            delete names; // 배열 내용 삭제
-            names = newNames;
-            // 숙제: 아래와 같이 구현 할수 있도록 연구해보기
-            // let names2 = name.distinct.list();
-            document.getElementById("viewName").value = "";
-            Index.draw();
-        }
-    },
-    validation: () => {
-        let inputName = document.getElementById("viewName").value;
-        let patt1 = /[0-9]/;
-        if (inputName === '') {
-            alert("이름을 입력해주세요");
-            return false;
-        } else if (inputName.match(patt1) !== null) {
-            alert("숫자를 입력하지마세요");
-            return false;
-        }
-        return true;
-    },
-    filterNames: (names, inputName) => {
-        // 숙제: filter, map으로 처리하는 방법 알아보기
-        // let resultFilterNames = names.map(i => {
-        //     let model = Index.setJsonDataToString(i);
-        //     if (model.name == inputName) {
-        //         continue;
-        //     }
-        // })
-        let resultFilterNames = [];
-        let doubleCount = 0;
-        for (let name of names) {
-            let model = Index.setStringToJsonData(name);
-            if (model.name == inputName) {
-                doubleCount++;
-            }
-            if (doubleCount > 1) {
-                continue;
-            }
-            let newModel = {
-                id: model.id,
-                name: model.name
-            };
-            resultFilterNames.push(Index.setJsonDataToString(newModel));
-        }
-        return resultFilterNames;
-    },
-    btnTotalClear: e => {
-        e.preventDefault();
-        names = [];
-        localStorage.clear();
-        Index.draw();
-    },
-    btnStart: e => {
-        e.preventDefault();
-        let tbodyHtml = "";
-        let newNames = Index.shuffle(names);
-        names = newNames;
-        names.map((name, i) => {
-            let model = Index.setStringToJsonData(name);
-            tbodyHtml += `
+      // 숙제: Set처리하는 방법 알아보기
+      // let setNames = new Set(names);
+      // names = Array.from(setNames);
+      // console.log(">>>>>" + Index.filterNames(names, Index.models.name));
+      // names = [];
+      let newNames = Index.filterNames(names, Index.models.name);
+      delete names; // 배열 내용 삭제
+      names = newNames;
+      // 숙제: 아래와 같이 구현 할수 있도록 연구해보기
+      // let names2 = name.distinct.list();
+      Index.setElementValue("viewName", "");
+      // document.getElementById("viewName").value = "";
+      Index.draw();
+    }
+  },
+  validation: () => {
+    let inputName = Index.getElementValue("viewName");
+    let patt1 = /[0-9]/;
+    if (inputName === "") {
+      alert("이름을 입력해주세요");
+      return false;
+    } else if (inputName.match(patt1) !== null) {
+      alert("숫자를 입력하지 마세요");
+      return false;
+    }
+    return true;
+  },
+  filterNames: (names, inputName) => {
+    // 숙제: filter, map으로 처리하는 방법 알아보기
+    // let resultFilterNames = names.map(i => {
+    //     let model = Index.setJsonDataToString(i);
+    //     if (model.name == inputName) {
+    //         continue;
+    //     }
+    // })
+    let resultFilterNames = [];
+    let doubleCount = 0;
+    for (let name of names) {
+      let model = Index.setStringToJsonData(name);
+      if (model.name === inputName) {
+        doubleCount++;
+      }
+      if (doubleCount > 1) {
+        continue;
+      }
+      let newModel = {
+        id: model.id,
+        name: model.name
+      };
+      resultFilterNames.push(Index.setJsonDataToString(newModel));
+    }
+    return resultFilterNames;
+  },
+  btnTotalClear: e => {
+    e.preventDefault();
+    // names = [];
+    localStorage.clear();
+    Index.draw();
+  },
+  btnStart: e => {
+    e.preventDefault();
+    let tbodyHtml = "";
+    // let newNames = Index.shuffle(names);
+
+    // 전역으로 접근 X 데드락 걸림 -> common.js 로 빼도록
+    // 리팩토링 필요 디자인 패턴을 알게됨
+    // 하나의 함수에 하나의 기능 필요
+    // 하나의 함수에서 다른 함수를 호출 하는 것이 순수함수는 아니다
+    let localNewNames = Index.shuffle(localStorage);
+    console.log("shuffle >>> " + localNewNames);
+    // console.log("shuffle >>> " + typeof localNewNames);
+    names = newNames;
+    names.map((name, i) => {
+      let model = Index.setStringToJsonData(name);
+      tbodyHtml += `
             <tr>
                 <td>${i + 1}</td>
                 <td>${model.id}</td>
                 <td>${model.name}</td>
                 <td>
                     <button onclick="Index.btnRowRemove('${
-                model.id
-                }',event);">삭제</button>
+                      model.id
+                    }',event);">삭제</button>
                     <button onclick="Index.btnRowEdit('${
-                model.name
-                }',event);">수정</button>
+                      model.name
+                    }',event);">수정</button>
                 </td>
             </tr>
         `;
-        });
-        document.querySelector("#historyList > tbody").innerHTML = tbodyHtml;
-    },
-    shuffle: array => {
-        var currentIndex = array.length,
-            temporaryValue,
-            randomIndex; // ...?
+    });
+    document.querySelector("#historyList > tbody").innerHTML = tbodyHtml;
+  },
+  shuffle: array => {
+    var currentIndex = array.length,
+      temporaryValue,
+      randomIndex; // ...?
+    // While there remain elements to shuffle...
+    // shuffle할 elements가 남아있는 동안
+    while (0 !== currentIndex) {
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
 
-        // While there remain elements to shuffle...
-        // shuffle할 elements가 남아있는 동안
-        while (0 !== currentIndex) {
-            // Pick a remaining element...
-            randomIndex = Math.floor(Math.random() * currentIndex);
-            currentIndex -= 1;
-
-            // And swap it with the current element.
-            // 현재 element와 바꾸기
-            temporaryValue = array[currentIndex];
-            array[currentIndex] = array[randomIndex];
-            array[randomIndex] = temporaryValue;
-        }
-        return array;
-    },
-    setJsonDataToString: data => JSON.stringify(data),
-    setStringToJsonData: data => JSON.parse(data),
-    uuid: () => {
-        function s4() {
-            return Math.floor((1 + Math.random()) * 0x10000)
-                .toString(16)
-                .substring(1);
-        }
-        return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
-    },
-    btnRowRemove: (modelId, e) => {
-        e.preventDefault();
-        names.map((name, i) => {
-            let model = Index.setStringToJsonData(name);
-            if (model.id == modelId) {
-                names.splice(i, 1);
-            }
-        });
-        Index.draw();
-    },
-    btnRowEdit: (modelName, e) => {
-        e.preventDefault();
-        let viewName = document.getElementById("viewName");
-        viewName.value = modelName;
-        viewName.focus();
-        let buttonHtml = `
+      // And swap it with the current element.
+      // 현재 element와 바꾸기
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+    return array;
+  },
+  setJsonDataToString: data => JSON.stringify(data),
+  setStringToJsonData: data => JSON.parse(data),
+  uuid: () => {
+    function s4() {
+      return Math.floor((1 + Math.random()) * 0x10000)
+        .toString(16)
+        .substring(1);
+    }
+    return (
+      s4() +
+      s4() +
+      "-" +
+      s4() +
+      "-" +
+      s4() +
+      "-" +
+      s4() +
+      "-" +
+      s4() +
+      s4() +
+      s4()
+    );
+  },
+  btnRowRemove: (modelId, e) => {
+    e.preventDefault();
+    names.map((name, i) => {
+      let model = Index.setStringToJsonData(name);
+      if (model.id == modelId) {
+        names.splice(i, 1);
+      }
+    });
+    Index.draw();
+  },
+  btnRowEdit: (modelName, e) => {
+    e.preventDefault();
+    let viewName = Index.getElement("viewName");
+    viewName.value = modelName;
+    viewName.focus();
+    let buttonHtml = `
             <button onclick="Index.btnRowEditComplete('${modelName}',event)">완료</button>
             <button onclick="Index.drawResetButton()">취소</button>
         `;
-        document.querySelector("form > div").innerHTML = buttonHtml;
-    },
-    btnRowEditComplete: (modelName, e) => {
-        e.preventDefault();
-        if (Index.validation()) {
-            let viewName = document.getElementById("viewName");
-            names.map((name, i) => {
-                let model = Index.setStringToJsonData(name);
-                console.log(model);
-                if (model.name == modelName) {
-                    model.name = viewName.value;
-                    name = Index.setJsonDataToString(model);
-                    names.splice(i, 1, name);
-                }
-            });
-            viewName.value = "";
-            Index.draw();
-            Index.drawResetButton();
+    document.querySelector("form > div").innerHTML = buttonHtml;
+  },
+  btnRowEditComplete: (modelName, e) => {
+    e.preventDefault();
+    if (Index.validation()) {
+      let viewName = Index.getElement("viewName");
+      names.map((name, i) => {
+        let model = Index.setStringToJsonData(name);
+        console.log(model);
+        if (model.name === modelName) {
+          model.name = viewName.value;
+          name = Index.setJsonDataToString(model);
+          names.splice(i, 1, name);
         }
-    },
-    /**
-     * TODO:
-     *  생성, 전체삭제, 복불복 toggle 처리
-     * */
-    drawResetButton: () => {
-        let buttonHtml = `
+      });
+      viewName.value = "";
+      Index.draw();
+      Index.drawResetButton();
+    }
+  },
+  /**
+   * TODO:
+   *  생성, 전체삭제, 복불복 toggle 처리
+   * */
+  drawResetButton: () => {
+    let buttonHtml = `
             <button onclick="Index.btnCreate(event);">생성</button>
             <button onclick="Index.btnTotalClear(event);">전체삭제</button>
             <button onclick="Index.btnStart(event);">복불복</button>
         `;
-        document.querySelector("form > div").innerHTML = buttonHtml;
-        document.getElementById("viewName").value = "";
-        Index.draw();
-    },
-    /*---------------------------------------------------
+    document.querySelector("form > div").innerHTML = buttonHtml;
+    Index.setElementValue("viewName", "");
+    // document.getElementById("viewName").value = "";
+    Index.draw();
+  },
+  /*---------------------------------------------------
     Array.prototype.sort()
     : 배열의 요소를 정렬하여 그 배열을 반환
     기본 정렬 순서는 문자열의 유니코드 코드 포인트를 따름.
@@ -210,70 +242,68 @@ let Index = {
     compareFunction(a, b) > 0 (return val = 1)
         - 정렬순서: b a
     -----------------------------------------------------*/
-    sortingName: () => {
-        let sortNames = [];
-        names.map((name) => {
-            let model = Index.setStringToJsonData(name);
-            sortNames.push(model);
-        });
-        // console.log(sortNames);
-        // console.log(typeof sortNames);      Object
-        // console.log(typeof sortNames.name); string
-        if(sort === "ascend") {
-            console.log("sort ascending");
-            // delete names; -> 안됌? 왜
-            names = [];
-            sortNames.sort(function(a, b) {
-                console.log(typeof a.name);
-                let nameA = a.name.toUpperCase();
-                let nameB = b.name.toUpperCase();
-                return nameA < nameB ? -1 : nameA > nameB ? 1 : 0;
-            });
-            sortNames.map((name, i) => {
-                names.push(Index.setJsonDataToString(name));
-            });
-            Index.draw();
-            sort = "descend";
-        } else {
-            console.log("sort descending");
-            names = [];
-            sortNames.sort(function(a, b) {
-                console.log(typeof a.name);
-                let nameA = a.name.toUpperCase();
-                let nameB = b.name.toUpperCase();
-                return nameA > nameB ? -1 : nameA < nameB ? 1 : 0;
-            });
-            sortNames.map((name, i) => {
-                names.push(Index.setJsonDataToString(name));
-            });
-            Index.draw();
-            sort = "ascend";
-        }
-    },
-    draw: () => {
-        let tbodyHtml = "";
-        for (let i=0; i<localStorage.length; i++) {
-            console.log("localStorage: " + localStorage.getItem(`index${i}`));
-            let data = Index.setStringToJsonData(localStorage.getItem(`index${i}`));
-            tbodyHtml += `
+  sortingName: () => {
+    let sortNames = [];
+    for (let i = 0; i < localStorage.length; i++) {
+      console.log("localStorage: " + localStorage.getItem(`index${i}`));
+      let data = Index.setStringToJsonData(localStorage.getItem(`index${i}`));
+      sortNames.push(data);
+    }
+    if (sort === "ascend") {
+      console.log("sort ascending");
+      sortNames.sort(function(a, b) {
+        let nameA = a.name.toUpperCase();
+        let nameB = b.name.toUpperCase();
+        return nameA < nameB ? -1 : nameA > nameB ? 1 : 0;
+      });
+      sortNames.map((name, i) => {
+        localStorage.setItem(`index${i}`, Index.setJsonDataToString(name));
+      });
+      Index.draw();
+      sort = "descend";
+    } else {
+      console.log("sort descending");
+      sortNames.sort(function(a, b) {
+        let nameA = a.name.toUpperCase();
+        let nameB = b.name.toUpperCase();
+        return nameA > nameB ? -1 : nameA < nameB ? 1 : 0;
+      });
+      sortNames.map((name, i) => {
+        localStorage.setItem(`index${i}`, Index.setJsonDataToString(name));
+      });
+      Index.draw();
+      sort = "ascend";
+    }
+  },
+  draw: () => {
+    let tbodyHtml = "";
+    for (let i = 0; i < localStorage.length; i++) {
+      let data = Index.setStringToJsonData(localStorage.getItem(`index${i}`));
+      console.log("localStorage: " + localStorage.getItem(`index${i}`));
+      console.log("data >>> " + JSON.stringify(data.id));
+      tbodyHtml += `
                 <tr>
                     <td></td>
                     <td>${data.id}</td>
                     <td>${data.name}</td>
                     <td>
-                        <button onclick="Index.btnRowRemove('${data.id}',event);">삭제</button>
-                        <button onclick="Index.btnRowEdit('${data.name}',event);">수정</button>
+                        <button onclick="Index.btnRowRemove('${
+                          data.id
+                        }',event);">삭제</button>
+                        <button onclick="Index.btnRowEdit('${
+                          data.name
+                        }',event);">수정</button>
                     </td>
                 </tr>
             `;
-        }
-        document.querySelector("#historyList > tbody").innerHTML = tbodyHtml;
-    },
-    event: () => {
-        console.log("draw");
-    },
-    models: {
-        id: "",
-        name: ""
     }
+    document.querySelector("#historyList > tbody").innerHTML = tbodyHtml;
+  },
+  event: () => {
+    console.log("draw");
+  },
+  models: {
+    id: "",
+    name: ""
+  }
 };
---
+++ study1202.js
@@ -0,0 +1,174 @@
+console.log("========================================================");
+console.log("\t\t\t로또");
+console.log("========================================================");
+
+let uncheckedAllTurns = [];
+//let tempTurns = [];
+let turns = [];
+
+function setNumRandom() {
+  let lottoNum = [];
+  for (let i = 0; i < 7; i++) {
+    lottoNum[i] = Math.floor(Math.random() * 38 + 1);
+  }
+  return lottoNum;
+}
+
+function checkNum(lottoNumber) {
+  let lottoCheck = new Set(lottoNumber);
+  let arrayLotto = [];
+  lottoCheck.forEach(function(value, key) {
+    arrayLotto.push(value);
+  });
+  return arrayLotto;
+}
+
+// TODO: 재귀 적용
+function checkTurns(allTurns) {
+  let lottoTurnCheck = new Set(allTurns);
+  lottoTurnCheck.forEach(function(value, key) {
+    turns.push(value);
+  });
+}
+
+function setTurn(checkedNum) {
+  checkedNum.sort(function(a, b) {
+    return a - b;
+  });
+  uncheckedAllTurns.push(JSON.stringify(checkedNum));
+}
+
+function* lottoGen(turns) {
+  for (let turn of turns) {
+    yield turn;
+  }
+}
+
+// 1~9회차 로또
+// 1 ~ 38
+function lottoFactory() {
+  for (let i = 0; i < 9; i++) {
+    // 루프로 7자리를 채우기
+    let randomNum = setNumRandom();
+
+    // 각 자리 수 마다 중복체크 (SET)
+    let checkedNum = checkNum(randomNum);
+
+    if (randomNum.length === checkedNum.length) {
+      // 모든 회차 array 담기
+      setTurn(checkedNum);
+    }
+  }
+  // 각 회차 마다 정렬 중복체크 (SET)
+  checkTurns(uncheckedAllTurns);
+
+  //   console.log("turns >>> " + turns);
+  //FIXME: turns가 9개로 채워질 때 까지 check해서 넣기
+
+  //TODO: 재귀함수 응용하면 Set 중복체크 관련 처리 가능
+}
+lottoFactory();
+
+// 결과 yield
+let lotto = lottoGen(turns);
+
+// 유지보수 확장성을 고려하자
+// 상태 관리 필요
+// map reduce filter 사용
+// -> lazy evolution
+
+function sumLotto() {
+  let lottoNum = lotto.next().value;
+  let sumLotto = JSON.parse(lottoNum);
+  let sum = 0;
+  for (let oneLotto of sumLotto) {
+    sum += oneLotto;
+  }
+  console.log("sum >>> " + sum);
+  return sum;
+}
+
+function compareSum(args) {
+  let sumArr = args.sort(function(a, b) {
+    return b - a;
+  });
+  console.log(sumArr);
+}
+
+// TODO: 로또 숫자의 합이 가장 큰 순서대로 출력
+let promise1 = new Promise(function(resolve, reject) {
+  setTimeout(function() {
+    resolve(sumLotto());
+  }, 1000);
+});
+
+let promise2 = new Promise(function(resolve, reject) {
+  setTimeout(function() {
+    resolve(sumLotto());
+  }, 2000);
+});
+
+let promise3 = new Promise(function(resolve, reject) {
+  setTimeout(function() {
+    resolve(sumLotto());
+  }, 3000);
+});
+
+let iterable = [promise1, promise2, promise3];
+Promise.all(iterable).then(
+  function(value) {
+    compareSum(value);
+    console.log("value >>> " + value);
+  },
+  function(reason) {
+    console.log(reason);
+  }
+);
+
+console.log("========================================================");
+console.log("\t\t\t로그인");
+console.log("========================================================");
+
+// Proxy를 이용해서 로그인 처리
+let member = {
+  userId: "admin",
+  password: "1234",
+  calculation: function() {
+    return "yes";
+  }
+};
+
+// DB
+let members = [
+  { user: "admin", pw: "1234" },
+  { user: "user01", pw: "1234" },
+  { user: "user02", pw: "1234" },
+  { user: "user03", pw: "1234" },
+  { user: "user04", pw: "1234" }
+];
+
+// CRUD
+let getLogin = function(member) {
+  members.filter(m => m.user == member.user);
+};
+
+// admin / 1234
+// id, pw가 일치하면 함수를 호출
+// get -> if (getLogin(member)) true else false;
+let proxy = new Proxy(member, {
+  set: function(target, prop, value, receiver) {
+    console.log(target);
+    target[prop] = value;
+    return true;
+  }
+});
+
+proxy.userId = "admin";
+proxy.password = "1234";
+console.log(proxy.userId);
+console.log(proxy.password);
+
+let bool = "userId" in proxy;
+console.log(bool);
+
+// member => proxy => state value static => call calc control
